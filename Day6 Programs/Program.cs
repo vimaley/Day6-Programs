4e0ec7b034f5043a85686d72926c0582f8e51a44
vimaley <Vimal.George1@gds.ey.com>
2023-02-21 07:56:25 +0000

    New Upload

--- Day6 Programs/Program.cs
+++ Day6 Programs/Program.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Day6_Programs
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int n;
+//namespace Day6_Programs
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n;
 
-            Console.WriteLine("Enter the number");
-            n = int.Parse(Console.ReadLine());
+//            Console.WriteLine("Enter the number");
+//            n = int.Parse(Console.ReadLine());
 
-            if (n == 0)
-                goto label1;
-            else
-                goto label2;
+//            if (n == 0)
+//                goto label1;
+//            else
+//                goto label2;
 
-            label1:
-            Console.WriteLine("The number is zero");
-            goto exitlabel;
+//            label1:
+//            Console.WriteLine("The number is zero");
+//            goto exitlabel;
 
-            label2:
-            Console.WriteLine("The number is not zero");
-            goto exitlabel;
+//            label2:
+//            Console.WriteLine("The number is not zero");
+//            goto exitlabel;
 
-            exitlabel:
-            Console.ReadLine();
+//            exitlabel:
+//            Console.ReadLine();
 
-        }
-    }
-}
+//        }
+//    }
+//}
