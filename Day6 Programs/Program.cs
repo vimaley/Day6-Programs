a3f93590093f41e1dbfb022bd6b02cd34a4f8147
vimaley <Vimal.George1@gds.ey.com>
2023-02-21 05:52:07 +0000

    Add project files.

---
+++ Day6 Programs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Enter the number");
+            n = int.Parse(Console.ReadLine());
+
+            if (n == 0)
+                goto label1;
+            else
+                goto label2;
+
+            label1:
+            Console.WriteLine("The number is zero");
+            goto exitlabel;
+
+            label2:
+            Console.WriteLine("The number is not zero");
+            goto exitlabel;
+
+            exitlabel:
+            Console.ReadLine();
+
+        }
+    }
+}
