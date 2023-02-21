d3b298fb3cc35e2526681cf40d37e716b16b0397
vimaley <Vimal.George1@gds.ey.com>
2023-02-21 09:31:32 +0000

    New upload

---
+++ Day6 Programs/2darr.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Day6_Programs
+//{
+//    class _2darr
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[,] arr = new int[2, 2];
+//            arr[0, 0] = 1;
+//            arr[0, 1] = 2;
+//            arr[1, 0] = 3;
+//            arr[1, 1] = 4;
+
+//            for(int i=0; i<2; i++)
+//            {
+//                for(int j=0; j<2; j++)
+//                {
+//                    Console.Write(arr[i, j] + "  ");
+//                }
+//                Console.WriteLine();
+//            } 
+            
+           
+//            Console.ReadLine();
+//        } 
+//    }
+//}
--- Day6 Programs/arr1.cs
+++ Day6 Programs/arr1.cs
@@ -1,30 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Day6_Programs
-{
-    class arr1
-    {
-        static void Main(string[] args)
-        {
-            int[] arr = new int[10];
-            int[] arr1 = new int[5] { 2, 3, 4, 5, 6 };
-            int[] arr2 = new int[6];
-            arr2[0] = 1;
-            arr2[1] = 2;
-            arr2[2] = 3;
-            arr2[3] = 4;
-            arr2[4] = 5;
+//namespace Day6_Programs
+//{
+//    class arr1
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arr = new int[10];
+//            int[] arr1 = new int[5] { 2, 3, 4, 5, 6 };
+//            int[] arr2 = new int[6];
+//            arr2[0] = 1;
+//            arr2[1] = 2;
+//            arr2[2] = 3;
+//            arr2[3] = 4;
+//            arr2[4] = 5;
 
-            foreach(int i in arr2)
-            {
-                Console.WriteLine(i);
-            }
+//            foreach(int i in arr2)
+//            {
+//                Console.WriteLine(i);
+//            }
 
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.ReadLine();
+//        }
+//    }
+//}
