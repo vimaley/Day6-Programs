87260f90d9881d5f3845c26d7783a4da77cf3a94
vimaley <Vimal.George1@gds.ey.com>
2023-02-21 10:11:26 +0000

    New upload

---
+++ Day6 Programs/addmat.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Programs
+{
+    class addmat
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter row size: ");
+            int rowsize = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter column size: ");
+            int colsize = int.Parse(Console.ReadLine());
+
+            int[,] arr = new int[rowsize, colsize];
+
+            Console.WriteLine("Enter 1st Array elements : ");
+            for (int i = 0; i < rowsize; i++)
+            {
+                for (int j = 0; j < colsize; j++)
+                {
+                    arr[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+
+            int[,] arr1 = new int[rowsize, colsize];
+
+            Console.WriteLine("Enter 2nd Array elements : ");
+            for (int i = 0; i < rowsize; i++)
+            {
+                for (int j = 0; j < colsize; j++)
+                {
+                    arr1[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int[,] arr2 = new int[rowsize, colsize];
+
+            for (int i = 0; i < rowsize; i++)
+            {
+                for (int j = 0; j < colsize; j++)
+                {
+                    arr2[i, j] = arr[i,j] + arr1[i,j];
+                }
+            }
+
+            Console.WriteLine("The sum of 2 matrix is: ");
+
+            for (int i = 0; i < rowsize; i++)
+            {
+                for (int j = 0; j < colsize; j++)
+                {
+                    Console.Write(arr2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
--- Day6 Programs/ipmatrixarr.cs
+++ Day6 Programs/ipmatrixarr.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Day6_Programs
-{
-    class ipmatrixarr
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter row size: ");
-            int rowsize = int.Parse(Console.ReadLine());
+//namespace Day6_Programs
+//{
+//    class ipmatrixarr
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter row size: ");
+//            int rowsize = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter column size: ");
-            int colsize = int.Parse(Console.ReadLine());
+//            Console.WriteLine("Enter column size: ");
+//            int colsize = int.Parse(Console.ReadLine());
 
-            int[,] arr = new int[rowsize, colsize];
+//            int[,] arr = new int[rowsize, colsize];
 
-            Console.WriteLine("Enter Array elements are: ");
-            for (int i = 0; i < rowsize; i++)
-            {
-                for (int j = 0; j < colsize; j++)
-                {
-                    arr[i, j] = int.Parse(Console.ReadLine());
-                }
-            }
+//            Console.WriteLine("Enter Array elements are: ");
+//            for (int i = 0; i < rowsize; i++)
+//            {
+//                for (int j = 0; j < colsize; j++)
+//                {
+//                    arr[i, j] = int.Parse(Console.ReadLine());
+//                }
+//            }
 
-            for (int i = 0; i < rowsize; i++)
-            {
-                for (int j = 0; j < colsize; j++)
-                {
-                    Console.Write(arr[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
+//            for (int i = 0; i < rowsize; i++)
+//            {
+//                for (int j = 0; j < colsize; j++)
+//                {
+//                    Console.Write(arr[i, j] + " ");
+//                }
+//                Console.WriteLine();
+//            }
 
-            Console.ReadLine();
+//            Console.ReadLine();
 
 
-        }
-    }
-}
+//        }
+//    }
+//}
