a556eaea160605f65f928dba40e3995b7ee32cf3
vimaley <Vimal.George1@gds.ey.com>
2023-02-22 10:02:07 +0000

    New Upload

--- Day6 Programs/htablepgm.cs
+++ Day6 Programs/htablepgm.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Day6_Programs
-{
-    class htablepgm
-    {
-        static void Main(string[] args)
-        {
-            Hashtable ht = new Hashtable();
-            ht.Add(1, "China");
-            ht.Add(2, "Japan");
-            ht.Add(3, "India");
+//namespace Day6_Programs
+//{
+//    class htablepgm
+//    {
+//        static void Main(string[] args)
+//        {
+//            Hashtable ht = new Hashtable();
+//            ht.Add(1, "China");
+//            ht.Add(2, "Japan");
+//            ht.Add(3, "India");
 
-            Console.WriteLine("Hash table elements are: ");
-            foreach(DictionaryEntry item in ht)
-            {
-                Console.WriteLine(item.Key + "   " + item.Value);
-            }
+//            Console.WriteLine("Hash table elements are: ");
+//            foreach(DictionaryEntry item in ht)
+//            {
+//                Console.WriteLine(item.Key + "   " + item.Value);
+//            }
 
-            Console.WriteLine("The no of elements in hastable is:" + ht.Count);
-            ht.Remove(2);
-            Console.WriteLine("The elements in hastable after removing is:");
-            foreach (DictionaryEntry item in ht)
-            {
-                Console.WriteLine(item.Key + "   " + item.Value);
-            }
-            Console.WriteLine(ht.ContainsValue("India"));
-            Console.WriteLine(ht.ContainsKey(1));
-            Console.WriteLine(ht.Contains(1));
-            if (ht.IsFixedSize == true)
-                Console.WriteLine("Size fixed");
-            else
-                Console.WriteLine("Size not fixed");
-            Console.ReadLine();
+//            Console.WriteLine("The no of elements in hastable is:" + ht.Count);
+//            ht.Remove(2);
+//            Console.WriteLine("The elements in hastable after removing is:");
+//            foreach (DictionaryEntry item in ht)
+//            {
+//                Console.WriteLine(item.Key + "   " + item.Value);
+//            }
+//            Console.WriteLine(ht.ContainsValue("India"));
+//            Console.WriteLine(ht.ContainsKey(1));
+//            Console.WriteLine(ht.Contains(1));
+//            if (ht.IsFixedSize == true)
+//                Console.WriteLine("Size fixed");
+//            else
+//                Console.WriteLine("Size not fixed");
+//            Console.ReadLine();
 
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ Day6 Programs/sortedlistpgm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Programs
+{
+    class sortedlistpgm
+    {
+        static void Main(string[] args)
+        {
+            SortedList s1 = new SortedList();
+            s1.Add(1, "China");
+            s1.Add(2, "Japan");
+            s1.Add(3, "India");
+            s1.Add(4, "Korea");
+            s1.Add(5, "USA");
+
+            Console.WriteLine("Hash table elements are: ");
+            foreach (DictionaryEntry item in s1)
+            {
+                Console.WriteLine(item.Key + "   " + item.Value);
+            }
+
+            s1.Remove(1);
+            s1.RemoveAt(2);
+            Console.WriteLine(s1.ContainsKey(5));
+            Console.WriteLine(s1.ContainsValue("Korea"));
+            Console.WriteLine(s1.Contains(5));
+            Console.ReadLine();
+        }
+    }
+}
