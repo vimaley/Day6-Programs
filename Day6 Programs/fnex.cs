892c036bf2cf143689ec444dc2289923847a849f
vimaley <Vimal.George1@gds.ey.com>
2023-02-22 11:52:25 +0000

    New Upload

---
+++ Day6 Programs/fnex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Programs
+{
+    class fnex
+    {
+        static void Main(string[] args)
+        {
+            fnex m1 = new fnex();
+            m1.add(10, 11);
+            Console.WriteLine("The sum of numbers is: " + m1.sum(9, 8));
+            Console.ReadLine();
+        }
+
+        public void add(int a, int b)
+        {
+            Console.WriteLine("The result of addition is: " + (a + b));
+        }
+        
+        public int sum(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
--- Day6 Programs/genfnmethodpgm.cs
+++ Day6 Programs/genfnmethodpgm.cs
@@ -1,35 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Day6_Programs
-{
-    class MyClass
-    {
-        static void Swap<T>(ref T num1, ref T num2)
-        {
-            T temp = default(T);
+//namespace Day6_Programs
+//{
+//    class MyClass
+//    {
+//        static void Swap<T>(ref T num1, ref T num2)
+//        {
+//            T temp = default(T);
 
-            temp = num2;
-            num2 = num1;
-            num1 = temp;
-        }
+//            temp = num2;
+//            num2 = num1;
+//            num1 = temp;
+//        }
 
-        static void Main(string[] args)
-        {
-            int first = 4;
-            int second = 5;
+//        static void Main(string[] args)
+//        {
+//            int first = 4;
+//            int second = 5;
 
-            Swap<int>(ref first, ref second);
+//            Swap<int>(ref first, ref second);
 
-            Console.WriteLine(first);
-            Console.WriteLine(second);
+//            Console.WriteLine(first);
+//            Console.WriteLine(second);
 
-            Console.ReadLine();
-        }
-    }
+//            Console.ReadLine();
+//        }
+//    }
 
     
-}
+//}
