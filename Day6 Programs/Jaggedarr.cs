12b97a9e1ad1634b71cccf6af7f25926b331bf00
vimaley <Vimal.George1@gds.ey.com>
2023-02-22 06:21:05 +0000

    New Upload

--- Day6 Programs/Jaggedarr.cs
+++ Day6 Programs/Jaggedarr.cs
@@ -1,26 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Day6_Programs
-{
-    class Jaggedarr
-    {
-        static void Main(string[] args)
-        {
-            int[][] arr = new int[2][];
-            arr[0] = new int[] { 1, 2, 3, 4 };
-            arr[1] = new int[] { 2, 3, 4 };
+//namespace Day6_Programs
+//{
+//    class Jaggedarr
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[][] arr = new int[4][];
+//            arr[0] = new int[] { 1, 2, 3, 4 };
+//            arr[1] = new int[] { 2, 3, 4 };
+//            arr[2] = new int[] { 11, 22, 31, 41 };
+//            arr[3] = new int[] { 8, 9, 3 };
 
-            for(int i=0; i<arr.Length; i++)
-            {
-                for(int j =0; j< arr[i].Length; j++)
-                {
-                    Console.WriteLine(arr[i][j] + "  ");
-                } Console.ReadLine();
-            }Console.ReadLine();
-        }
-    }
-}
+//            for(int i=0; i<arr.Length; i++)
+//            {
+//                for(int j =0; j< arr[i].Length; j++)
+//                {
+//                    Console.WriteLine(arr[i][j] + "  ");
+//                } Console.ReadLine();
+//            }Console.ReadLine();
+//        }
+//    }
+//}
---
+++ Day6 Programs/Stackpgm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Programs
+{
+    class Stackpgm
+    {
+        static void Main(string[] args)
+        {
+            Stack s1 = new Stack();
+            s1.Push(1);
+            s1.Push(22);
+            s1.Push(3);
+            s1.Push(34);
+            s1.Push(45);
+            s1.Push(67);
+            s1.Push(57);
+            s1.Push(89);
+
+            Console.WriteLine("Stack elements are: ");
+            foreach(int i in s1)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("The count of elements in Stack: " + s1.Count);
+            s1.Pop();
+            Console.WriteLine("The topmost element in stack is: " + s1.Peek());
+
+            Stack s2 = new Stack();
+            s2.Push("Vimal");
+            s2.Push(2.343f);
+            s2.Push(1);
+
+            Console.WriteLine("Elements in Stack2: ");
+
+            foreach(var i in s2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
