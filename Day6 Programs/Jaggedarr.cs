dc33e2e091a11326baa8b4d4f87ca1cf3fb3d7b0
vimaley <Vimal.George1@gds.ey.com>
2023-02-22 04:37:51 +0000

    New Upload

---
+++ Day6 Programs/Jaggedarr.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Programs
+{
+    class Jaggedarr
+    {
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[2][];
+            arr[0] = new int[] { 1, 2, 3, 4 };
+            arr[1] = new int[] { 2, 3, 4 };
+
+            for(int i=0; i<arr.Length; i++)
+            {
+                for(int j =0; j< arr[i].Length; j++)
+                {
+                    Console.WriteLine(arr[i][j] + "  ");
+                } Console.ReadLine();
+            }Console.ReadLine();
+        }
+    }
+}
---
+++ Day6 Programs/arrfns.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Day6_Programs
+//{
+//    class arrfns
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arr = new int[10] { 1, 11, 9, 8, 5, 4, 3, 2, 7, 6 };
+//            Console.WriteLine("Sorted Array: ");
+//            Array.Sort(arr);
+//            for(int i=0;i<10;i++)
+//            {
+//                Console.WriteLine(arr[i]);
+//            }
+//            Console.WriteLine("Reverse Array: ");
+//            Array.Reverse(arr);
+//            Console.WriteLine("Length of Array: " + arr.Length);
+//            Console.ReadLine();
+//        }
+//    }
+//}
