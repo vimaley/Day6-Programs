97ef056f9e5fb1186d75369b7d41669095ba885c
vimaley <Vimal.George1@gds.ey.com>
2023-02-22 10:26:09 +0000

    New Upload

---
+++ Day6 Programs/genericfnpgm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Programs
+{
+    class A<T>
+    {
+        public A(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A<string> gen = new A<string>("This is genric class");
+            A<int> genI = new A<int>(101);
+            A<char> gench = new A<char>('I');
+
+            Console.ReadLine();
+        }
+    }
+}
