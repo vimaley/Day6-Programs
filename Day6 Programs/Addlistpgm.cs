d51bef88aea823526320a3bd6c769d9b42af1336
vimaley <Vimal.George1@gds.ey.com>
2023-02-22 07:13:54 +0000

    New upload

---
+++ Day6 Programs/Addlistpgm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Programs
+{
+    class Addlistpgm
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList();
+            arr.Add(1);
+            arr.Add(22);
+            arr.Add(33);
+            arr.Add(44);
+            arr.Add(15);
+            arr.Add(29);
+            arr.Add(25);
+            arr.Add(100);
+
+            Console.WriteLine("Arraylist elements are: ");
+            foreach(var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            arr.Sort();
+            Console.WriteLine("Arraylist elements after sort are: ");
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            arr.Reverse();
+            Console.WriteLine("Arraylist elements after reversing are: ");
+                foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            arr.Remove(1); //1 value will be removed from the array list
+            arr.RemoveAt(0); // 0th index value will be removed
+            arr.RemoveRange(0, 2); //from the 0th index 2 values will be removed
+
+            Console.WriteLine("Arraylist after removing elements: ");
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
--- Day6 Programs/queuepgm.cs
+++ Day6 Programs/queuepgm.cs
@@ -1,50 +1,50 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Day6_Programs
-{
-    class queuepgm
-    {
-        static void Main(string[] args)
-        {
-            Queue s1 = new Queue();
-            s1.Enqueue(1);
-            s1.Enqueue(22);
-            s1.Enqueue(3);
-            s1.Enqueue(34);
-            s1.Enqueue(45);
-            s1.Enqueue(67);
-            s1.Enqueue(57);
-            s1.Enqueue(89);
+//namespace Day6_Programs
+//{
+//    class queuepgm
+//    {
+//        static void Main(string[] args)
+//        {
+//            Queue s1 = new Queue();
+//            s1.Enqueue(1);
+//            s1.Enqueue(22);
+//            s1.Enqueue(3);
+//            s1.Enqueue(34);
+//            s1.Enqueue(45);
+//            s1.Enqueue(67);
+//            s1.Enqueue(57);
+//            s1.Enqueue(89);
 
-            Console.WriteLine("Queue elements are: ");
-            foreach (int i in s1)
-            {
-                Console.WriteLine(i);
-            }
+//            Console.WriteLine("Queue elements are: ");
+//            foreach (int i in s1)
+//            {
+//                Console.WriteLine(i);
+//            }
 
-            Console.WriteLine("The count of elements in Queue: " + s1.Count);
-            s1.Dequeue();
-            Console.WriteLine("The topmost element in Queue is: " + s1.Peek());
+//            Console.WriteLine("The count of elements in Queue: " + s1.Count);
+//            s1.Dequeue();
+//            Console.WriteLine("The topmost element in Queue is: " + s1.Peek());
 
-            Queue s2 = new Queue();
-            s2.Enqueue("Vimal");
-            s2.Enqueue(2.343f);
-            s2.Enqueue(1);
+//            Queue s2 = new Queue();
+//            s2.Enqueue("Vimal");
+//            s2.Enqueue(2.343f);
+//            s2.Enqueue(1);
 
-            Console.WriteLine("Elements in Queue2: ");
+//            Console.WriteLine("Elements in Queue2: ");
 
-            foreach (var i in s2)
-            {
-                Console.WriteLine(i);
-            }
+//            foreach (var i in s2)
+//            {
+//                Console.WriteLine(i);
+//            }
 
-            Console.ReadLine();
+//            Console.ReadLine();
 
-        }
-    }
-}
+//        }
+//    }
+//}
