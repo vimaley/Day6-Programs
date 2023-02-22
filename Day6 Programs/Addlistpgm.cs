028dc618b9435e36923bc3e73b49965c4d5fc5e1
vimaley <Vimal.George1@gds.ey.com>
2023-02-22 09:40:26 +0000

    New Upload

--- Day6 Programs/Addlistpgm.cs
+++ Day6 Programs/Addlistpgm.cs
@@ -1,76 +1,76 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Day6_Programs
-{
-    class Addlistpgm
-    {
-        static void Main(string[] args)
-        {
-            ArrayList arr = new ArrayList();
-            arr.Add(1);
-            arr.Add(22);
-            arr.Add(33);
-            arr.Add(44);
-            arr.Add(15);
-            arr.Add(29);
-            arr.Add(25);
-            arr.Add(100);
+//namespace Day6_Programs
+//{
+//    class Addlistpgm
+//    {
+//        static void Main(string[] args)
+//        {
+//            ArrayList arr = new ArrayList();
+//            arr.Add(1);
+//            arr.Add(22);
+//            arr.Add(33);
+//            arr.Add(44);
+//            arr.Add(15);
+//            arr.Add(29);
+//            arr.Add(25);
+//            arr.Add(100);
 
-            Console.WriteLine("Arraylist elements are: ");
-            foreach(var item in arr)
-            {
-                Console.WriteLine(item);
-            }
+//            Console.WriteLine("Arraylist elements are: ");
+//            foreach(var item in arr)
+//            {
+//                Console.WriteLine(item);
+//            }
 
-            arr.Sort();
-            Console.WriteLine("Arraylist elements after sort are: ");
-            foreach (var item in arr)
-            {
-                Console.WriteLine(item);
-            }
+//            arr.Sort();
+//            Console.WriteLine("Arraylist elements after sort are: ");
+//            foreach (var item in arr)
+//            {
+//                Console.WriteLine(item);
+//            }
 
-            arr.Reverse();
-            Console.WriteLine("Arraylist elements after reversing are: ");
-                foreach (var item in arr)
-            {
-                Console.WriteLine(item);
-            }
+//            arr.Reverse();
+//            Console.WriteLine("Arraylist elements after reversing are: ");
+//                foreach (var item in arr)
+//            {
+//                Console.WriteLine(item);
+//            }
 
-            arr.Remove(1); //1 value will be removed from the array list
-            arr.RemoveAt(0); // 0th index value will be removed
-            arr.RemoveRange(0, 2); //from the 0th index 2 values will be removed
+//            arr.Remove(1); //1 value will be removed from the array list
+//            arr.RemoveAt(0); // 0th index value will be removed
+//            arr.RemoveRange(0, 2); //from the 0th index 2 values will be removed
 
-            Console.WriteLine("Arraylist after removing elements: ");
-            foreach (var item in arr)
-            {
-                Console.WriteLine(item);
-            }
+//            Console.WriteLine("Arraylist after removing elements: ");
+//            foreach (var item in arr)
+//            {
+//                Console.WriteLine(item);
+//            }
 
             
 
-            ArrayList arr1 = new ArrayList();
-            arr1.Add(10);
-            arr1.Add(40);
-            arr1.Add(50);
+//            ArrayList arr1 = new ArrayList();
+//            arr1.Add(10);
+//            arr1.Add(40);
+//            arr1.Add(50);
 
-            arr.AddRange(arr1);
+//            arr.AddRange(arr1);
 
-            Console.WriteLine("Array elements after adding another array elements");
+//            Console.WriteLine("Array elements after adding another array elements");
 
-            foreach (var item in arr)
-            {
-                Console.WriteLine(item);
-            }
+//            foreach (var item in arr)
+//            {
+//                Console.WriteLine(item);
+//            }
 
-            Console.ReadLine();
+//            Console.ReadLine();
 
 
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ Day6 Programs/htablepgm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Programs
+{
+    class htablepgm
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add(1, "China");
+            ht.Add(2, "Japan");
+            ht.Add(3, "India");
+
+            Console.WriteLine("Hash table elements are: ");
+            foreach(DictionaryEntry item in ht)
+            {
+                Console.WriteLine(item.Key + "   " + item.Value);
+            }
+
+            Console.WriteLine("The no of elements in hastable is:" + ht.Count);
+            ht.Remove(2);
+            Console.WriteLine("The elements in hastable after removing is:");
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine(item.Key + "   " + item.Value);
+            }
+            Console.WriteLine(ht.ContainsValue("India"));
+            Console.WriteLine(ht.ContainsKey(1));
+            Console.WriteLine(ht.Contains(1));
+            if (ht.IsFixedSize == true)
+                Console.WriteLine("Size fixed");
+            else
+                Console.WriteLine("Size not fixed");
+            Console.ReadLine();
+
+
+        }
+    }
+}
